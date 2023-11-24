864d5a794fbb2dc13930ab563d7afabb4ac63b51
John Falcón <johngrowscode@gmail.com>
2023-11-23 23:30:40 -0600

    change: middleware disabled for now

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,14 +13,14 @@ export function middleware(request: NextRequest) {
   // Call our authentication function to check the request
   const seshToken = fetchToken();
   console.log("where fetch token", seshToken);
-  if (! seshToken ) {
-    // Respond with JSON indicating an error message
-    // return NextResponse.json(
-    //   { success: false, message: 'Need authentication to be here, cowboy' },
-    //   { status: 401 }
-    // )
-    // const url = `${process.env.NEXT_PUBLIC_BASE_URL}401`;
-    // return NextResponse.redirect(url);
-  }
+  // if (! seshToken ) {
+  //   // Respond with JSON indicating an error message
+  //   return NextResponse.json(
+  //     { success: false, message: 'Need authentication to be here, cowboy' },
+  //     { status: 401 }
+  //   )
+  //   const url = `${process.env.NEXT_PUBLIC_BASE_URL}401`;
+  //   return NextResponse.redirect(url);
+  // }
 
 }
